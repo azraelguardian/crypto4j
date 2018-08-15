43a42c9f92d26d08408448ce3ddb37e07a7cedac
Sean Pan <seanpan2008@gmail.com>
2018-08-15 16:03:51 +0800

    1

--- crypto4j-demo/src/main/java/io/github/xinyangpan/crypto4j/demo/configuration/DemoConfiguration.java
+++ crypto4j-demo/src/main/java/io/github/xinyangpan/crypto4j/demo/configuration/DemoConfiguration.java
@@ -3,21 +3,11 @@ package io.github.xinyangpan.crypto4j.demo.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.github.xinyangpan.crypto4j.binance.rest.BinanceRestService;
 import io.github.xinyangpan.crypto4j.core.RestProperties;
 import io.github.xinyangpan.crypto4j.huobi.rest.HuobiRestService;
 
 @Configuration
 public class DemoConfiguration {
-
-	@Bean
-	public BinanceRestService binanceRestService() {
-		RestProperties restProperties = new RestProperties();
-		restProperties.setRestBaseUrl("https://api.binance.com");
-//		restProperties.setRestKey(Crypto4jUtils.getSecret("binance.key"));
-//		restProperties.setRestSecret(Crypto4jUtils.getSecret("binance.secret"));
-		return new BinanceRestService(restProperties);
-	}
 
 	@Bean
 	public HuobiRestService huobiRestService() {
