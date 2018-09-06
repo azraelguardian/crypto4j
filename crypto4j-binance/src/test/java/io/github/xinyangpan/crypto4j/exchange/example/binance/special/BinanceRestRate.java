283e771e8e891032680a1e591262cd3c56918b91
Sean Pan <seanpan2008@gmail.com>
2018-09-06 20:32:35 +0800

    fix

--- crypto4j-binance/src/test/java/io/github/xinyangpan/crypto4j/exchange/example/binance/special/BinanceRestRate.java
+++ crypto4j-binance/src/test/java/io/github/xinyangpan/crypto4j/exchange/example/binance/special/BinanceRestRate.java
@@ -11,14 +11,14 @@ import io.github.xinyangpan.crypto4j.binance.dto.rest.order.QueryOrderRequest;
 import io.github.xinyangpan.crypto4j.binance.rest.BinanceRestService;
 import io.github.xinyangpan.crypto4j.exchange.example.binance.util.BinanceTestUtils;
 
-public class RestExample {
+public class BinanceRestRate {
 
 	private static final String BTCUSDT = "BTCUSDT";
 	private static final BinanceRestService binanceRestService = BinanceTestUtils.binanceService().restService();
 
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService executorService = Executors.newFixedThreadPool(1000);
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 1000; i++) {
 			int j = i;
 			executorService.submit(() -> {
 				String key = binanceRestService.getRestProperties().getRestKey();
@@ -34,9 +34,9 @@ public class RestExample {
 					message = e.getMessage();
 					e.printStackTrace();
 					out = System.err;
-//					if (e.getStatusCode().value() == 418) {
-//						throw e;
-//					}
+					if (e.getStatusCode().value() == 418) {
+						throw e;
+					}
 				} catch (Exception e) {
 					message = e.getMessage();
 					e.printStackTrace();
