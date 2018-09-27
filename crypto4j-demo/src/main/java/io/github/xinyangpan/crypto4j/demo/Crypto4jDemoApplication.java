7d32b4868083b5ece19d1a1e8858d54725cc1d1a
Sean Pan <seanpan2008@gmail.com>
2018-09-27 16:26:33 +0800

    1

--- crypto4j-demo/src/main/java/io/github/xinyangpan/crypto4j/demo/Crypto4jDemoApplication.java
+++ crypto4j-demo/src/main/java/io/github/xinyangpan/crypto4j/demo/Crypto4jDemoApplication.java
@@ -24,7 +24,7 @@ public class Crypto4jDemoApplication {
 	public static OkexRestService okexRestService;
 	static {
 		RestProperties restProperties = new RestProperties();
-		restProperties.setRestBaseUrl("https://www.okex.com");
+		restProperties.setRestBaseUrl("https://104.25.20.25");
 		restProperties.setRestKey(Crypto4jUtils.getSecret("/home/panxy/okex.key"));
 		restProperties.setRestSecret(Crypto4jUtils.getSecret("/home/panxy/okex.secret"));
 		//  
@@ -36,9 +36,12 @@ public class Crypto4jDemoApplication {
 //		ConfigurableApplicationContext context = SpringApplication.run(Crypto4jDemoApplication.class, args);
 //		HuobiWsKlineProcess huobiWsKlineProcess = context.getBean(HuobiWsKlineProcess.class);
 //		huobiWsKlineProcess.start();
-		int total = Integer.parseInt(args[0]);
+		int total = 1;
+		if (args.length > 0) {
+			total = Integer.parseInt(args[0]);
+		}
 		for (int i = 0; i < total; i++) {
-			System.out.println(LocalDateTime.now() + " - " + okexRestService.userinfo());
+			System.out.println(String.format("%s - %s - %s", i, LocalDateTime.now(), okexRestService.userinfo()));
 		}
 	}
 
