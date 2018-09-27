d7a72de1b650fb523e522f1de0608e36d583c3af
Sean Pan <seanpan2008@gmail.com>
2018-09-27 16:06:30 +0800

    demo

--- crypto4j-demo/src/main/java/io/github/xinyangpan/crypto4j/demo/Crypto4jDemoApplication.java
+++ crypto4j-demo/src/main/java/io/github/xinyangpan/crypto4j/demo/Crypto4jDemoApplication.java
@@ -1,16 +1,18 @@
 package io.github.xinyangpan.crypto4j.demo;
 
-import org.springframework.boot.SpringApplication;
+import java.time.LocalDateTime;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import io.github.xinyangpan.crypto4j.core.RestProperties;
+import io.github.xinyangpan.crypto4j.core.util.Crypto4jUtils;
 import io.github.xinyangpan.crypto4j.demo.persist.KlinePo;
-import io.github.xinyangpan.crypto4j.demo.service.HuobiWsKlineProcess;
+import io.github.xinyangpan.crypto4j.okex.rest.OkexRestService;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = { KlinePo.class, Jsr310JpaConverters.class })
@@ -19,11 +21,24 @@ import io.github.xinyangpan.crypto4j.demo.service.HuobiWsKlineProcess;
 @EnableScheduling
 public class Crypto4jDemoApplication {
 
+	public static OkexRestService okexRestService;
+	static {
+		RestProperties restProperties = new RestProperties();
+		restProperties.setRestBaseUrl("https://www.okex.com");
+		restProperties.setRestKey(Crypto4jUtils.getSecret("/home/panxy/okex.key"));
+		restProperties.setRestSecret(Crypto4jUtils.getSecret("/home/panxy/okex.secret"));
+		//  
+		okexRestService = new OkexRestService(restProperties);
+	}
+	
 	public static void main(String[] args) {
 		// 
-		ConfigurableApplicationContext context = SpringApplication.run(Crypto4jDemoApplication.class, args);
-		HuobiWsKlineProcess huobiWsKlineProcess = context.getBean(HuobiWsKlineProcess.class);
-		huobiWsKlineProcess.start();
+//		ConfigurableApplicationContext context = SpringApplication.run(Crypto4jDemoApplication.class, args);
+//		HuobiWsKlineProcess huobiWsKlineProcess = context.getBean(HuobiWsKlineProcess.class);
+//		huobiWsKlineProcess.start();
+		for (int i = 0; i < 6; i++) {
+			System.out.println(LocalDateTime.now() + " - " + okexRestService.userinfo());
+		}
 	}
 
 }
