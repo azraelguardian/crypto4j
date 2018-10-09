d2cdb77be8edee0004c6b5d1ab888cef7524d9c5
Sean Pan <seanpan2008@gmail.com>
2018-10-09 19:15:44 +0800

    1

--- crypto4j-okex/src/main/java/io/github/xinyangpan/crypto4j/okex/dto/enums/OrderType.java
+++ crypto4j-okex/src/main/java/io/github/xinyangpan/crypto4j/okex/dto/enums/OrderType.java
@@ -2,4 +2,13 @@ package io.github.xinyangpan.crypto4j.okex.dto.enums;
 
 public enum OrderType {
 	buy, sell, buy_market, sell_market;
+
+	public boolean isLimit() {
+		return this == buy || this == sell;
+	}
+
+	public boolean isMarket() {
+		return this == buy_market || this == sell_market;
+	}
+
 }
