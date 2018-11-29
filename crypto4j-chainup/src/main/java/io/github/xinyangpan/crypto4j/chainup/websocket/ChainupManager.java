c01cde5d1b5cb72819722b77422a28667270bbe7
Sean Pan <seanpan2008@gmail.com>
2018-11-29 19:04:11 +0800

    fix

--- crypto4j-chainup/src/main/java/io/github/xinyangpan/crypto4j/chainup/websocket/ChainupManager.java
+++ crypto4j-chainup/src/main/java/io/github/xinyangpan/crypto4j/chainup/websocket/ChainupManager.java
@@ -2,10 +2,10 @@ package io.github.xinyangpan.crypto4j.chainup.websocket;
 
 import java.time.LocalDateTime;
 
-import io.github.xinyangpan.crypto4j.chainup.websocket.impl.ChainupSubscriber;
+import io.github.xinyangpan.crypto4j.chainup.websocket.impl.ChainupSubscriber1;
 import io.github.xinyangpan.crypto4j.core.websocket.WebSocketManager;
 
-public class ChainupManager extends WebSocketManager<ChainupSubscriber> {
+public class ChainupManager extends WebSocketManager<ChainupSubscriber1> {
 	private static String DEFAULT_URL = "wss://ws.hiex.pro/kline-api/ws";
 
 	public ChainupManager() {
--- crypto4j-chainup/src/test/java/io/github/xinyangpan/crypto4j/chainup/example/ChainupConnectExample.java
+++ crypto4j-chainup/src/test/java/io/github/xinyangpan/crypto4j/chainup/example/ChainupConnectExample.java
@@ -3,24 +3,25 @@ package io.github.xinyangpan.crypto4j.chainup.example;
 import java.util.concurrent.ExecutionException;
 
 import io.github.xinyangpan.crypto4j.chainup.websocket.ChainupManager;
-import io.github.xinyangpan.crypto4j.chainup.websocket.impl.ChainupSubscriber;
+import io.github.xinyangpan.crypto4j.chainup.websocket.impl.ChainupSubscriber1;
 
 public class ChainupConnectExample {
 	
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		ChainupSubscriber chainupSubscriber = new ChainupSubscriber();
-		chainupSubscriber.setDepthListener(System.out::println);
-		chainupSubscriber.setTradeListener(System.out::println);
-		chainupSubscriber.setTickListener(System.out::println);
-		chainupSubscriber.setDepthListener(depth -> {System.out.println(String.format("ask size=%s, bid size=%s", depth.getTick().getAsks().size(), depth.getTick().getBids().size()));});
+		ChainupSubscriber1 chainupSubscriber1 = new ChainupSubscriber1();
+		chainupSubscriber1.setDepthListener(System.out::println);
+		chainupSubscriber1.setTradeListener(System.out::println);
+		chainupSubscriber1.setTickListener(System.out::println);
+		chainupSubscriber1.setDepthListener(depth -> {System.out.println(String.format("ask size=%s, bid size=%s", depth.getTick().getAsks().size(), depth.getTick().getBids().size()));});
 //		chainupSubscriber.tick("btcusdt");
 //		chainupSubscriber.trade("btcusdt");
-		chainupSubscriber.depth("btcusdt", 5);
+		chainupSubscriber1.depth("btcusdt", 5);
 		// 
 		ChainupManager connector = new ChainupManager();
 		// 54.95.129.249
-		connector.setUrl("wss://ws.hiex.pro/kline-api/ws");
-		connector.setSubscriber(chainupSubscriber);
+		connector.setUrl("ws://dev5ws.chaindown.com/kline-api/ws");
+//		connector.setUrl("wss://ws.hiex.pro/kline-api/ws");
+		connector.setSubscriber(chainupSubscriber1);
 		connector.connect();
 		// 
 		Thread.sleep(Long.MAX_VALUE);
