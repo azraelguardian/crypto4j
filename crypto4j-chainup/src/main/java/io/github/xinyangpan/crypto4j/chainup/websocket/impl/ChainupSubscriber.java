979498d8693a103720277c0e770aa03274e777b1
Sean Pan <seanpan2008@gmail.com>
2018-11-29 19:19:54 +0800

    chainup

--- crypto4j-chainup/src/main/java/io/github/xinyangpan/crypto4j/chainup/websocket/impl/ChainupSubscriber.java
+++ crypto4j-chainup/src/main/java/io/github/xinyangpan/crypto4j/chainup/websocket/impl/ChainupSubscriber.java
@@ -3,16 +3,27 @@ package io.github.xinyangpan.crypto4j.chainup.websocket.impl;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.function.Consumer;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
 import com.google.common.base.Preconditions;
 
+import io.github.xinyangpan.crypto4j.chainup.dto.ws.event.Event;
+import io.github.xinyangpan.crypto4j.chainup.dto.ws.event.tick.DepthTick;
+import io.github.xinyangpan.crypto4j.chainup.dto.ws.event.tick.Tick;
+import io.github.xinyangpan.crypto4j.chainup.dto.ws.event.tick.TradeTick;
+import io.github.xinyangpan.crypto4j.chainup.dto.ws.sub.EventAck;
 import io.github.xinyangpan.crypto4j.chainup.dto.ws.sub.EventSub;
 import io.github.xinyangpan.crypto4j.chainup.dto.ws.sub.params.DepthParam;
 import io.github.xinyangpan.crypto4j.chainup.dto.ws.sub.params.Param;
+import io.github.xinyangpan.crypto4j.core.util.Crypto4jUtils;
 import io.github.xinyangpan.crypto4j.core.websocket.Subscriber;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,13 +34,62 @@ import lombok.extern.slf4j.Slf4j;
 @Setter
 public class ChainupSubscriber extends Subscriber {
 
-	protected String getTextMessage(ByteBuffer payload) throws IOException {
+	private final static TypeReference<EventAck> ACK = new TypeReference<EventAck>() {};
+	private final static TypeReference<Event<TradeTick>> TRADE = new TypeReference<Event<TradeTick>>() {};
+	private final static TypeReference<Event<DepthTick>> DEPTH = new TypeReference<Event<DepthTick>>() {};
+	private final static TypeReference<Event<Tick>> TICK = new TypeReference<Event<Tick>>() {};
+
+	private Consumer<Event<TradeTick>> tradeListener = Crypto4jUtils.logConsumer();
+	private Consumer<Event<DepthTick>> depthListener = Crypto4jUtils.logConsumer();
+	private Consumer<Event<Tick>> tickListener = Crypto4jUtils.logConsumer();
+
+	@Override
+	protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+		String jsonMessage = this.getTextMessage(message.getPayload());
+		log.debug(MSG_TRACK, "{}: Handling message: {}", this.getName(), jsonMessage);
+		JsonNode root = objectMapper.readTree(jsonMessage);
+		// ping
+		JsonNode eventNode = root.findValue("ping");
+		if (eventNode != null) {
+			onPing(jsonMessage);
+			return;
+		}
+		// ack
+		eventNode = root.findValue("event_rep");
+		if (eventNode != null && "subed".equals(eventNode.textValue())) {
+			onAck(objectMapper.readValue(jsonMessage, ACK));
+			return;
+		}
+		// channel message
+		String channel = root.findValue("channel").asText();
+		if (channel.endsWith("_trade_ticker")) {
+			Event<TradeTick> event = objectMapper.readValue(jsonMessage, TRADE);
+			tradeListener.accept(event);
+			return;
+		} else if (channel.endsWith("_ticker")) {
+			Event<Tick> event = objectMapper.readValue(jsonMessage, TICK);
+			tickListener.accept(event);
+			return;
+		} else if (channel.contains("_depth_step")) {
+			Event<DepthTick> event = objectMapper.readValue(jsonMessage, DEPTH);
+			depthListener.accept(event);
+			return;
+		}
+		// 
+		this.unhandledMessage(jsonMessage);
+	}
+	
+	private String getTextMessage(ByteBuffer payload) throws IOException {
 		GZIPInputStream gzipInputStream = new GZIPInputStream(new ByteBufferBackedInputStream(payload));
 		String json = IOUtils.toString(gzipInputStream, Charset.forName("utf-8"));
 		return json;
 	}
+	
+	private void onAck(EventAck eventAck) {
+		log.info("Ack<{}>: {}", this.getName(), eventAck);
+	}
 
-	protected void onPing(String jsonMessage) {
+	private void onPing(String jsonMessage) {
 		log.debug("{}: Ping - {}", this.getName(), jsonMessage);
 	}
 
--- crypto4j-chainup/src/test/java/io/github/xinyangpan/crypto4j/chainup/example/ChainupConnectExample.java
+++ crypto4j-chainup/src/test/java/io/github/xinyangpan/crypto4j/chainup/example/ChainupConnectExample.java
@@ -3,25 +3,24 @@ package io.github.xinyangpan.crypto4j.chainup.example;
 import java.util.concurrent.ExecutionException;
 
 import io.github.xinyangpan.crypto4j.chainup.websocket.ChainupManager;
-import io.github.xinyangpan.crypto4j.chainup.websocket.impl.ChainupSubscriber1;
+import io.github.xinyangpan.crypto4j.chainup.websocket.impl.ChainupSubscriber;
 
 public class ChainupConnectExample {
 	
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		ChainupSubscriber1 chainupSubscriber1 = new ChainupSubscriber1();
-		chainupSubscriber1.setDepthListener(System.out::println);
-		chainupSubscriber1.setTradeListener(System.out::println);
-		chainupSubscriber1.setTickListener(System.out::println);
-		chainupSubscriber1.setDepthListener(depth -> {System.out.println(String.format("ask size=%s, bid size=%s", depth.getTick().getAsks().size(), depth.getTick().getBids().size()));});
-//		chainupSubscriber.tick("btcusdt");
-//		chainupSubscriber.trade("btcusdt");
-		chainupSubscriber1.depth("btcusdt", 5);
+		ChainupSubscriber chainupSubscriber = new ChainupSubscriber();
+		chainupSubscriber.setDepthListener(System.out::println);
+		chainupSubscriber.setTradeListener(System.out::println);
+		chainupSubscriber.setTickListener(System.out::println);
+		chainupSubscriber.tick("btcusdt");
+		chainupSubscriber.trade("btcusdt");
+		chainupSubscriber.depth("btcusdt", 5);
 		// 
 		ChainupManager connector = new ChainupManager();
 		// 54.95.129.249
 		connector.setUrl("ws://dev5ws.chaindown.com/kline-api/ws");
 //		connector.setUrl("wss://ws.hiex.pro/kline-api/ws");
-		connector.setSubscriber(chainupSubscriber1);
+		connector.setSubscriber(chainupSubscriber);
 		connector.connect();
 		// 
 		Thread.sleep(Long.MAX_VALUE);
