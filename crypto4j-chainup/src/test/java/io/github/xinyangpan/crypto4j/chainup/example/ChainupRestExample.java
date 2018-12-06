8e4cf3dca8254d1903891825441c4f3edd857286
Sean Pan <seanpan2008@gmail.com>
2018-12-06 22:30:18 +0800

    1

--- crypto4j-chainup/src/test/java/io/github/xinyangpan/crypto4j/chainup/example/ChainupRestExample.java
+++ crypto4j-chainup/src/test/java/io/github/xinyangpan/crypto4j/chainup/example/ChainupRestExample.java
@@ -72,8 +72,8 @@ public class ChainupRestExample {
 	public static void main(String[] args) throws Exception {
 		//		System.out.println(chainupRestService.getAllSymbols());
 		//		System.out.println(chainupRestService.getTick(HIEXXFNH));
-//		System.out.println(chainupRestService.getAccountInfo());
-		System.out.println(chainupRestService.getAllOrder(BTCUSDT));
+		System.out.println(chainupRestService.getAccountInfo());
+//		System.out.println(chainupRestService.getAllOrder(BTCUSDT));
 //				System.out.println(chainupRestService.createOrder(createOrder()));
 		//		System.out.println(chainupRestService.cancelOrder(8198, BTCUSDT));
 //				System.out.println(chainupRestService.getOrderInfo(25765, HIEXXFNH));
